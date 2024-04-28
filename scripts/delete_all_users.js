bf650f7a0db904fb586ba6d04d0c6551a6897541
Othman Shawgan <oth1998@gmail.com>
2024-04-28 22:52:25 +0300

    Update script for delete all users

--- scripts/delete_all_users.js
+++ scripts/delete_all_users.js
@@ -1,30 +1,22 @@
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
 // Initialize Firebase Admin SDK
-admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-  // Replace 'YOUR_PROJECT_ID' with your Firebase project ID
-  databaseURL: 'https://wrestle-predict.firebaseio.com',
-});
+admin.initializeApp();
 
-// Get all users
-admin
-  .auth()
-  .listUsers()
-  .then((listUsersResult) => {
+// HTTP function to delete all users
+exports.deleteAllUsers = functions.https.onRequest(async (req, res) => {
+  try {
+    // Get all users
+    const listUsersResult = await admin.auth().listUsers();
     // Iterate through each user and delete them
-    listUsersResult.users.forEach((userRecord) => {
-      admin
-        .auth()
-        .deleteUser(userRecord.uid)
-        .then(() => {
-          console.log(`Successfully deleted user: ${userRecord.uid}`);
-        })
-        .catch((error) => {
-          console.error(`Error deleting user: ${userRecord.uid}`, error);
-        });
-    });
-  })
-  .catch((error) => {
-    console.error('Error listing users:', error);
-  });
+    await Promise.all(
+        // eslint-disable-next-line max-len
+        listUsersResult.users.map((userRecord) => admin.auth().deleteUser(userRecord.uid)),
+    );
+    res.status(200).send("All users have been deleted.");
+  } catch (error) {
+    console.error("Error deleting users:", error);
+    res.status(500).send("An error occurred while deleting users.");
+  }
+});
