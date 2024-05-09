614207180842c9aab961d9151f1a1265f3b0c371
Othman Shawgan <oth1998@gmail.com>
2024-05-09 23:26:20 +0300

    Add users field to season

--- lib/services/admin_service.dart
+++ lib/services/admin_service.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:multi_select_flutter/multi_select_flutter.dart';
 import 'package:uuid/uuid.dart';
 import 'package:wrestle_predict/models/event_model.dart';
+import 'package:wrestle_predict/models/user_model.dart';
 
 import '../models/season.dart';
 import '../models/match.dart';
@@ -11,6 +13,8 @@ final FirestoreService fs = FirestoreService();
 
 showNewSeasonDialog(BuildContext context) async {
   final TextEditingController seasonNameController = TextEditingController(text: "");
+  List<UserModel> users;
+  List<UserModel> selectedUsers = [];
 
   var confirmMethod = (() async {
     fs.getAllUsers().then((users) {
@@ -22,6 +26,7 @@ showNewSeasonDialog(BuildContext context) async {
         events: [],
         leaderboard: leaderboard,
         isActive: true,
+        users: selectedUsers.map((user) => user.uid).toList(),
       );
 
       fs.addSeason(newSeason);
@@ -50,25 +55,58 @@ showNewSeasonDialog(BuildContext context) async {
         ),
       ],
       content: StatefulBuilder(builder: (BuildContext context, StateSetter setState) {
-        return Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(24.0, 16.0, 24.0, 16.0),
-              child: Container(
-                constraints: const BoxConstraints(minWidth: 400, maxWidth: 600, maxHeight: 100, minHeight: 50),
-                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                child: TextFormField(
-                  controller: seasonNameController,
-                  decoration: const InputDecoration(
-                    labelText: 'Season Name',
-                    border: OutlineInputBorder(),
+        return FutureBuilder(
+          future: fs.getAllUsers(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) return const LinearProgressIndicator();
+            users = snapshot.data!;
+            final _items =
+                users.map((user) => MultiSelectItem<UserModel>(user, "${user.firstName} ${user.lastName}")).toList();
+
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(24.0, 16.0, 24.0, 16.0),
+                  child: Container(
+                    constraints: const BoxConstraints(minWidth: 400, maxWidth: 600, maxHeight: 100, minHeight: 50),
+                    padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                    child: TextFormField(
+                      controller: seasonNameController,
+                      decoration: const InputDecoration(
+                        labelText: 'Season Name',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-          ],
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(24.0, 16.0, 24.0, 0),
+                  child: Container(
+                      constraints: const BoxConstraints(minWidth: 400, maxWidth: 600, maxHeight: 120, minHeight: 50),
+                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                      child: MultiSelectDialogField<UserModel>(
+                        title: const Text('All Users'),
+                        buttonText: const Text('Select Users'),
+                        chipDisplay: MultiSelectChipDisplay(scroll: true),
+                        dialogHeight: MediaQuery.of(context).size.height * 0.4,
+                        dialogWidth: MediaQuery.of(context).size.width * 0.4,
+                        confirmText: Text('Confirm',
+                            style: TextStyle(color: Theme.of(context).buttonTheme.colorScheme!.primary)),
+                        cancelText:
+                            Text('Cancel', style: TextStyle(color: Theme.of(context).buttonTheme.colorScheme!.primary)),
+                        itemsTextStyle: TextStyle(color: Colors.grey.shade500),
+                        selectedItemsTextStyle: const TextStyle(color: Colors.white),
+                        items: _items,
+                        onConfirm: (values) {
+                          selectedUsers = values;
+                        },
+                      )),
+                ),
+              ],
+            );
+          },
         );
       }));
 
